211424d56e9a86eb919f06aa6ca4eaece378b5b9
SUN <suunn001@gmail.com>
2024-12-22 22:02:20 +0900

    Lv1 1

---
+++ Test_Lv1_1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Test_Lv1_1 {
+       /* 함수 solution은 정수 n을 매개변수로 입력받습니다.
+        n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
+        예를들어 n이 118372면 873211을 리턴하면 됩니다.*/
+
+        public static void main(String[] args) {
+            long n = 118372;
+            long answer = 0;
+            String[] str = String.valueOf(n).split("");
+            Arrays.sort(str);
+
+            StringBuilder sb = new StringBuilder();
+            for (String string : str) {
+                sb.append(string);
+            }
+            StringBuilder reverse = sb.reverse();
+            answer = Long.parseLong(reverse.toString());
+            System.out.println(answer);
+        }
+    }
+
