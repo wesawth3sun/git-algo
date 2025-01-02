79810ec3c9ef38688dbaf08f8b4864605fbee49e
SUN <suunn001@gmail.com>
2025-01-02 22:39:24 +0900

    new

--- chat/server/command/Command.java
+++
@@ -1,9 +0,0 @@
-package chat.server.command;
-
-import chat.server.Session;
-
-import java.io.IOException;
-
-public interface Command {
-    void execute(String[] args, Session session) throws IOException;
-}
--- wiseSaying/App.java
+++
@@ -1,8 +0,0 @@
-package wiseSaying;
-
-public class App {
-    public void run() {
-        System.out.println("== 명언 앱 ==");
-        System.out.println("명령) 종료");
-    }
-}
