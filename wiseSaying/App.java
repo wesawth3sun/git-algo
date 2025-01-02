886335895bd16ed7e49ac08d684215a65a4dca5e
SUN <suunn001@gmail.com>
2025-01-02 22:54:50 +0900

    while문 도입

--- wiseSaying/App.java
+++ wiseSaying/App.java
@@ -1,8 +1,24 @@
 package wiseSaying;
 
+import java.util.Scanner;
+
 public class App {
     public void run() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) 종료");
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+            if (command.equals("등록")) {
+                System.out.print("명언: ");
+                String wiseSaying = scanner.nextLine();
+                System.out.print("작가: ");
+                String author = scanner.nextLine();
+                System.out.println("명언: " + wiseSaying +
+                        ", 작가: " + author);
+            } else if (command.equals("종료")) {
+                break;
+            }
+        }
     }
 }
